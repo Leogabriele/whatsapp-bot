93a6ad53f48a9ef1dceb2a7381ac17563b51b1cc
Jegan Nadar <88388487+Leogabriele@users.noreply.github.com>
2026-04-09 12:17:57 +0530

    Update Appointment.js

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,1 +1,11 @@
+const mongoose = require("mongoose");
 
+const AppointmentSchema = new mongoose.Schema({
+  phone: String,
+  name: String,
+  doctor: String,
+  date: String,
+  time: String,
+});
+
+module.exports = mongoose.model("Appointment", AppointmentSchema);
