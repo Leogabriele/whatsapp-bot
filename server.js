5937d7c8fd3ae8b88f8a7e4d47b473643d3e8764
Jegan Nadar <88388487+Leogabriele@users.noreply.github.com>
2026-04-09 12:18:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./db");
 
+const app = express();
+connectDB();
+
+app.use(bodyParser.json());
+
+app.use("/webhook", require("./routes/webhook"));
+
+app.get("/", (req, res) => res.send("Bot Running"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
